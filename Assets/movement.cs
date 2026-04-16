78c6a3f0264c813ac0d6b064baa7fe180634a44b
Alexgordino <afanshaw95@gmail.com>
2026-04-16 16:03:38 +0100

    fewdasd

---
+++ Assets/movement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float movespeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
